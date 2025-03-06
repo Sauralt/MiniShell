2e83ea4f9f4dce9f577c28fadb9ac54192828277
Corentin <cf.fleuret@gmail.com>
2025-03-06 17:16:50 +0100

    petites modifs

--- Include/minishell.h
+++ Include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: cfleuret <cfleuret@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/31 15:01:25 by cfleuret          #+#    #+#             */
-/*   Updated: 2025/03/06 16:33:08 by cfleuret         ###   ########.fr       */
+/*   Updated: 2025/03/06 16:37:26 by cfleuret         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,8 +54,8 @@ typedef struct s_shell
 int		main(int argc, char **argv, char **env);
 int		init_tokens(t_shell *data, char *line);
 void	set_token_type(t_shell *data, char *line, int type);
-t_token	*ft_stacknew(char *content);
-void	ft_stackadd(t_token **s, t_token *new);
-void	delfirst(t_stack **s);
+t_token	*ft_new_token(char *content);
+void	ft_add_token(t_token **s, t_token *new);
+void	delfirst(t_token **s);
 
 #endif
