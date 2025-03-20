fb70ea70c84bd95ae70ff77ef7e735fe292ca167
Corentin <cf.fleuret@gmail.com>
2025-03-20 15:06:15 +0100

    Changement encore de structure

--- srcs/exec/process.c
+++ srcs/exec/process.c
@@ -6,66 +6,63 @@
 /*   By: cfleuret <cfleuret@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/12 15:58:48 by cfleuret          #+#    #+#             */
-/*   Updated: 2025/03/19 16:41:00 by cfleuret         ###   ########.fr       */
+/*   Updated: 2025/03/20 14:20:48 by cfleuret         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-// static int	exec_abs(char *cmd, t_env *env)
-// {
-// 	char	*path;
-// 	char	**envp;
-// 	char	**cmds;
-// 	int		i;
+static int	exec_abs(char **cmd, t_env *env)
+{
+	char	*path;
+	char	**envp;
+	int		i;
 
-// 	i = 0;
+	i = 0;
 
-// 	cmds = ft_split(cmd, ' ');
-// 	envp = make_env_str(env);
-// 	path = find_path(cmds[0], env, i);
-// 	if (!path)
-// 	{
-// 		printf("%s: command not found\n", cmd);
-// 		return (1);
-// 	}
-// 	if (execve(path, cmds, envp) == -1)
-// 	{
-// 		free(path);
-// 		printf("%s: command not found\n", cmd);
-// 		return (1);
-// 	}
-// 	free(path);
-// 	free(cmds);
-// 	free_str(envp);
-// 	return (0);
-// }
+	envp = make_env_str(env);
+	path = find_path(cmd[0], env, i);
+	if (!path)
+	{
+		printf("%s: command not found\n", cmd[0]);
+		return (1);
+	}
+	if (execve(path, cmd, envp) == -1)
+	{
+		free(path);
+		printf("%s: command not found\n", cmd[0]);
+		return (1);
+	}
+	free(path);
+	free_str(envp);
+	return (0);
+}
 
 static void	exec_built(t_shell *data, t_token *cmd)
 {
 	if (!cmd || !cmd->str)
 		return ;
 
-	if (ft_strncmp(cmd->str, "pwd", 4) == 0)
+	if (ft_strncmp(cmd->str[0], "pwd", 4) == 0)
 		ft_pwd(data);
-	else if (ft_strncmp(cmd->str, "env", 4) == 0)
+	else if (ft_strncmp(cmd->str[0], "env", 4) == 0)
 		ft_env(data);
-	else if (ft_strncmp(cmd->str, "cd", 3) == 0 && cmd->next == cmd->prev)
-		ft_cd(data, cmd->next->str);
-	else if (ft_strncmp(cmd->str, "echo", 5) == 0)
-		ft_echo(cmd->next->str);
-	else if (ft_strncmp(cmd->str, "export", 7) == 0)
-		ft_export(cmd->str);
-//	else if (ft_strncmp(cmd->str, "unset", 6) == 0)
+	else if (ft_strncmp(cmd->str[0], "cd", 3) == 0 && cmd->next == cmd->prev)
+		ft_cd(data, cmd->next->str[0]);
+	else if (ft_strncmp(cmd->str[0], "echo", 5) == 0)
+		ft_echo(cmd->next->str[0]);
+	else if (ft_strncmp(cmd->str[0], "export", 7) == 0)
+		ft_export(cmd->str[0]);
+//	else if (ft_strncmp(cmd->str[0], "unset", 6) == 0)
 //		ft_unset(cmd->next->str);
 }
 
 static int	builtin(t_shell *data, t_token *cmd)
 {
-	if (ft_strcmp(cmd->str, "echo") == 0 || ft_strcmp(cmd->str, "cd") == 0
-		|| ft_strcmp(cmd->str, "pwd") == 0 || ft_strcmp(cmd->str, "export") == 0
-		|| ft_strcmp(cmd->str, "unset") == 0 || ft_strcmp(cmd->str, "env") == 0
-		|| ft_strcmp(cmd->str, "exit") == 0)
+	if (ft_strcmp(cmd->str[0], "echo") == 0 || ft_strcmp(cmd->str[0], "cd") == 0
+		|| ft_strcmp(cmd->str[0], "pwd") == 0 || ft_strcmp(cmd->str[0], "export") == 0
+		|| ft_strcmp(cmd->str[0], "unset") == 0 || ft_strcmp(cmd->str[0], "env") == 0
+		|| ft_strcmp(cmd->str[0], "exit") == 0)
 	{
 		exec_built(data, cmd);
 		return (0);
@@ -77,7 +74,7 @@ int	proc(t_shell *data)
 {
 	if (data->token->type == 0)
 	{
-		printf("command not found : %s\n", data->token->str);
+		printf("command not found : %s\n", data->token->str[0]);
 		return (0);
 	}
 	if (data->token->type == 2 && data->token->next == data->token)
@@ -87,7 +84,7 @@ int	proc(t_shell *data)
 	}
 	if (builtin(data, data->token) == 1)
 	{
-		//exec_abs(data->token->str, data->env);
+		exec_abs(data->token->str, data->env);
 		printf("temp\n");
 	}
 	return (0);
--- srcs/utils/token_functions.c
+++ srcs/utils/token_functions.c
@@ -6,7 +6,7 @@
 /*   By: cfleuret <cfleuret@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/18 12:34:34 by cfleuret          #+#    #+#             */
-/*   Updated: 2025/03/11 14:53:33 by cfleuret         ###   ########.fr       */
+/*   Updated: 2025/03/20 14:38:20 by cfleuret         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,16 @@ t_token	*ft_new_token(char *content)
 	c = malloc(sizeof(*c));
 	if (c == NULL)
 		return (NULL);
-	c->str = ft_strdup(content);
+	c->str = malloc(sizeof(char *) * 2);
+	if (!c->str)
+		return (NULL);
+	c->str[0] = ft_strdup(content);
+	if (!c->str[0])
+	{
+		free(c->str);
+		return (NULL);
+	}
+	c->str[1] = NULL;
 	c->next = c;
 	c->prev = c;
 	c->type = -1;
@@ -65,4 +74,33 @@ void	delfirst(t_token **s)
 	t = *s;
 	*s = (*s)->next;
 	free(t);
+}
+
+int	add_param(t_shell *data, int i, char **str)
+{
+	t_token	*t;
+	int		j;
+
+	j = -1;
+	t = data->token;
+	while (j++ < i - 1)
+		t = t->next;
+	if (t->type == 1)
+	{
+		while (strncmp(str[j], "-", 1) == 0)
+			j++;
+		free_str(t->str);
+		t->str = malloc(sizeof(char *) * (j + 1));
+		t->str[0] = ft_strdup(str[i - 1]);
+		j = 1;
+		while (strncmp(str[i], "-", 1) == 0)
+		{
+			t->str[j] = ft_strdup(str[i]);
+			j++;
+			i++;
+		}
+		printf("2\n");
+		t->str[j] = NULL;
+	}
+	return (i);
 }
