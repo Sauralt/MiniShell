0b4c772699860f9435108ecf926ad2b6568738ab
BjornSauralt <maxime.garsault@gmail.com>
2025-04-17 17:10:57 +0200

    tg

--- srcs/exec/heredoc.c
+++ srcs/exec/heredoc.c
@@ -6,7 +6,7 @@
 /*   By: mgarsaul <mgarsaul@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/31 16:48:41 by cfleuret          #+#    #+#             */
-/*   Updated: 2025/04/17 15:43:36 by mgarsaul         ###   ########.fr       */
+/*   Updated: 2025/04/17 17:02:43 by mgarsaul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,18 @@ char	*ft_strjoin_free(char *s1, char *s2)
 {
 	char	*res;
 
-	if (!s1 && !s2)
-		return (NULL);
+	if (!s1 && s2)
+	{
+		res = ft_strdup(s2);
+		free(s2);
+		return (res);
+	}
+	if (!s2 && s1)
+	{
+		res = ft_strdup(s1);
+		free(s1);
+		return (res);
+	}
 	if (!s1)
 		return (s2);
 	if (!s2)
