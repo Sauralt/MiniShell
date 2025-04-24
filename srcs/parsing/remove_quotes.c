223cf89e3c9d2045a6d465aa250ae3e498c40167
Corentin <cf.fleuret@gmail.com>
2025-04-24 13:11:13 +0200

    save

--- srcs/parsing/remove_quotes.c
+++ srcs/parsing/remove_quotes.c
@@ -6,7 +6,7 @@
 /*   By: cfleuret <cfleuret@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/24 11:50:08 by cfleuret          #+#    #+#             */
-/*   Updated: 2025/04/24 12:53:28 by cfleuret         ###   ########.fr       */
+/*   Updated: 2025/04/24 13:10:58 by cfleuret         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,32 +47,62 @@ char	find_quote(char *s, int start, int len)
 	return (quote);
 }
 
-// static char	*no_dollar(char *s, int start, int len, char *temp)
-// {
-// 	char	*quote;
-// 	char	*str;
-// 	int		i;
-// 	int		j;
+static char	*is_dollar(char *s, int start, int len, char *temp)
+{
+	int		i;
+	int		j;
+	char	*str;
+	char	quote;
 
-// 	i = 0;
-// 	j = 0;
-// 	str = malloc((real_len(s, start, len) + 1) * sizeof(char));
-// 	if (!str)
-// 		return (s);
-// 	i = start;
-// 	while (i < start + len)
-// 	{
-// 		if (temp[i] == quote)
-// 			i++;
-// 		else
-// 		{
-// 			str[j] = temp[i];
-// 			i++;
-// 			j++;
-// 		}
-// 	}
-// 	return (str);
-// }
+	i = 0;
+	j = 0;
+	str = malloc((real_len(temp, 0, ft_strlen(temp), quote) + 1)
+			* sizeof(char));
+	if (!str)
+		return (s);
+	i = 0;
+	while (temp[i])
+	{
+		if (temp[i] == quote)
+			i++;
+		else
+		{
+			str[j] = temp[i];
+			i++;
+			j++;
+		}
+	}
+	str[j] = '\0';
+	return (str);
+}
+
+static char	*no_dollar(char *s, int start, int len, char *temp)
+{
+	char	quote;
+	char	*str;
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	str = malloc((real_len(s, start, len, quote) + 1) * sizeof(char));
+	if (!str)
+		return (s);
+	i = start;
+	while (i < start + len)
+	{
+		if (temp[i] == quote)
+			i++;
+		else
+		{
+			str[j] = temp[i];
+			i++;
+			j++;
+		}
+	}
+	str[j] = '\0';
+	return (str);
+}
 
 char	*ft_strndup_no_quote(char *s, int start, int len, t_shell *data)
 {
@@ -86,43 +116,9 @@ char	*ft_strndup_no_quote(char *s, int start, int len, t_shell *data)
 	quote = find_quote(s, start, len);
 	temp = ft_strdup(init_nstr(data, s, start, len));
 	if (strcmp(temp, s) == 0)
-	{
-		str = malloc((real_len(s, start, len, quote) + 1) * sizeof(char));
-		if (!str)
-			return (s);
-		i = start;
-		while (i < start + len)
-		{
-			if (temp[i] == quote)
-				i++;
-			else
-			{
-				str[j] = temp[i];
-				i++;
-				j++;
-			}
-		}
-	}
+		str = no_dollar(s, start, len, temp);
 	else
-	{
-		str = malloc((real_len(temp, 0, ft_strlen(temp), quote) + 1)
-				* sizeof(char));
-		if (!str)
-			return (s);
-		i = 0;
-		while (temp[i])
-		{
-			if (temp[i] == quote)
-				i++;
-			else
-			{
-				str[j] = temp[i];
-				i++;
-				j++;
-			}
-		}
-	}
+		str = is_dollar(s, start, len, temp);
 	free(temp);
-	str[j] = '\0';
 	return (str);
 }
