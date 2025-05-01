310ee7d671c65141d93e9f0fff2a59858452d4ad
BjornSauralt <maxime.garsault@gmail.com>
2025-05-01 15:43:55 +0200

    $? ta mere

--- srcs/parsing/dollar_2.c
+++ srcs/parsing/dollar_2.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   dollar_2.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cfleuret <cfleuret@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mgarsaul <mgarsaul@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/29 12:54:12 by mgarsaul          #+#    #+#             */
-/*   Updated: 2025/05/01 14:57:40 by cfleuret         ###   ########.fr       */
+/*   Updated: 2025/05/01 15:28:25 by mgarsaul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,11 @@
 
 static int	is_valid_dollar(char *str, int i, int *f)
 {
+	if (str[i] == '$' && str[i + 1] == '?')
+	{
+		*f = 2;
+		return (1);
+	}
 	if (str[i] == '$' && ft_isdigit(str[i + 1]))
 	{
 		(*f) = 1;
@@ -51,6 +56,33 @@ static char	*extract_dollar_key(char *str, int *i, t_shell *data, int flag)
 	return (ft_strndup(str, start, end - start));
 }
 
+// static void	fill_var_array(t_shell *data, char **var, char *str, int *index)
+// {
+// 	int		i;
+// 	int		j;
+// 	int		f;
+// 	int		quote;
+// 	char	*temp;
+
+// 	i = index[0];
+// 	j = index[1];
+// 	quote = 0;
+// 	while (i < data->start + data->l)
+// 	{
+// 		quote = quote_flag(quote, str, i);
+// 		if (is_valid_dollar(str, i, &f) && quote != 1)
+// 		{
+// 			temp = extract_dollar_key(str, &i, data, f);
+// 			var[j++] = ft_dollar(data, temp);
+// 		}
+// 		else
+// 			i++;
+// 	}
+// 	index[0] = i;
+// 	index[1] = j;
+// 	var[j] = NULL;
+// }
+
 static void	fill_var_array(t_shell *data, char **var, char *str, int *index)
 {
 	int		i;
@@ -67,6 +99,12 @@ static void	fill_var_array(t_shell *data, char **var, char *str, int *index)
 		quote = quote_flag(quote, str, i);
 		if (is_valid_dollar(str, i, &f) && quote != 1)
 		{
+			if (f == 2)
+			{
+				i += 2;
+				var[j++] = ft_itoa(data->exit_code);
+				continue ;
+			}
 			temp = extract_dollar_key(str, &i, data, f);
 			var[j++] = ft_dollar(data, temp);
 		}
